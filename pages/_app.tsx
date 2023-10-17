4e6f9a97f53e8e71fa1b37604ce4714db51d0768
안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>
2023-10-17 16:30:55 +0900

    #5 chore: apollo 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app";
 import { Global } from "@emotion/react";
-import ApolloSetting from "@/src/components/commons/apollo";
 import Layout from "@/src/components/commons/layout";
 import { globalStyles } from "@/src/commons/styles/globalStyles";
 import { Roboto } from "next/font/google";
@@ -9,15 +8,13 @@ const roboto = Roboto({ weight: ["400", "500", "700"], subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ApolloSetting>
-      <>
-        <Global styles={globalStyles} />
-        <main className={roboto.className}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </main>
-      </>
-    </ApolloSetting>
+    <>
+      <Global styles={globalStyles} />
+      <main className={roboto.className}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
+    </>
   );
 }
