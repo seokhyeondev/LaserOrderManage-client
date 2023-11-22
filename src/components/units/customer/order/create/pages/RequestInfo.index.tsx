b79203ebed435ef8619a9c020b1f6fbdbbd67297
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-11-22 16:41:11 +0900

    #9 feat: 거래 생성 요청사항 상태 저장

--- src/components/units/customer/order/create/pages/RequestInfo.index.tsx
+++ src/components/units/customer/order/create/pages/RequestInfo.index.tsx
@@ -1,10 +1,36 @@
 import Spacer from "@/src/components/commons/spacer/Spacer.index";
 import * as S from "../CreateOrder.styles";
+import { useEffect } from "react"
 import { useInputWithMaxLength } from "@/src/lib/hooks/useInput";
 import { ICreateOrderPageProps } from "../CreateOrder.types";
+import { useRecoilState } from "recoil";
+import { createOrderState } from "@/src/store/createOrder"
 
 export default function RequestInfo(props: ICreateOrderPageProps) {
-  const [request, onChangeRequest] = useInputWithMaxLength(200);
+  const requestArgs = useInputWithMaxLength(200);
+  const [orderState, setOrderState] = useRecoilState(createOrderState);
+
+  useEffect(() =>{
+    requestArgs.setValue(orderState.request);
+  }, []);
+
+  const setCreateOrderState = () => {
+    setOrderState({
+      ...orderState,
+      request: requestArgs.value
+    });
+  }
+
+  const onNext = () => {
+    setCreateOrderState();
+    if(props.onNext) props.onNext();
+  }
+
+  const onBefore = () => {
+    setCreateOrderState();
+    if(props.onBefore) props.onBefore();
+  }
+
   return (
     <S.FormWrapper className="flex-column">
       <S.FormBodyWrapper>
@@ -13,24 +39,24 @@ export default function RequestInfo(props: ICreateOrderPageProps) {
         <S.FormTextArea
           className="regular16"
           placeholder="요청사항을 입력해주세요"
-          value={request}
+          value={requestArgs.value}
           maxLength={200}
-          onChange={onChangeRequest}
+          onChange={requestArgs.onChange}
         />
         <Spacer width="100%" height="8px" />
         <S.FormInputLength className="regular14 flex-column-end">
-          {`${request.length}/200`}
+          {`${requestArgs.value.length}/200`}
         </S.FormInputLength>
       </S.FormBodyWrapper>
       <S.FormButtonWrapper className="flex-column-end">
         <div className="flex-row">
-          <S.BackButton className="bold20" onClick={props.onBefore}>
+          <S.BackButton className="bold20" onClick={onBefore}>
             이전
           </S.BackButton>
           <S.NextButton
             className="bold20"
             enabled={true}
-            onClick={props.onNext}
+            onClick={onNext}
           >
             다음
           </S.NextButton>
