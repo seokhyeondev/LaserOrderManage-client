565146693fbabfbe4aeda8325f4aacc1f3568656
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-11-24 13:30:08 +0900

    #11 feat: 회원가입 관련 api 선언

--- src/lib/apis/user/UserApi.ts
+++ src/lib/apis/user/UserApi.ts
@@ -1,5 +1,5 @@
 import { axiosPrivate, axiosPublic } from "../axios";
-import { ILoginRequest, IToken } from "./User.types";
+import { IJoinRequest, ILoginRequest, IRequestVerifyRequest, IRequestVerifyResponse, IToken, IVerifyEmailRequest, IVerifyEmailResponse } from "./User.types";
 
 export const UserApi = {
   LOGIN: async (payload: ILoginRequest): Promise<IToken> => {
@@ -7,13 +7,23 @@ export const UserApi = {
     return response.data;
   },
   REISSUE: async (): Promise<IToken> => {
-    const response = await axiosPrivate.post("/user/re-issue", {
-      withCredentials: true,
-    });
+    const response = await axiosPrivate.post("/user/re-issue");
     return response.data;
   },
   LOGOUT: async (): Promise<null> => {
-    const response = await axiosPrivate.post("user/logout");
+    const response = await axiosPrivate.post("/user/logout");
     return response.data;
   },
+  REQUEST_VERIFY: async (payload: IRequestVerifyRequest): Promise<IRequestVerifyResponse> => {
+    const response = await axiosPrivate.post("/user/request-verify", payload);
+    return response.data;
+  },
+  VERIFY_EMAIL: async (payload: IVerifyEmailRequest): Promise<IVerifyEmailResponse> => {
+    const response = await axiosPrivate.post("/user/verify-email", payload);
+    return response.data;
+  },
+  JOIN: async (payload: IJoinRequest): Promise<IJoinRequest> => {
+    const response = await axiosPrivate.post("/user/join/customer", payload);
+    return response.data;
+  }
 };
