640b34f75b3bc3318a8674593910b03a48ae1079
안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>
2023-11-28 12:59:47 +0900

    #11 feat: 거래 완료 목록 조회 기능 연동

--- src/components/commons/modal/create/LoadOrderModal.index.tsx
+++ src/components/commons/modal/create/LoadOrderModal.index.tsx
@@ -2,31 +2,51 @@ import Spacer from "../../spacer/Spacer.index";
 import Modal from "../Modal.index";
 import * as S from "./LoadOrderModal.styles";
 import Image from "next/image";
-import { useState } from "react"
+import { useState } from "react";
 import { IModalProps } from "../Modal.index";
 import LoadOrderSearchbar from "../../searchbars/create/LoadOrderSearchbar";
 import { useSearchbar } from "@/src/lib/hooks/useSearchBar";
-import { IOrderHistory, IOrderHistoryResponse } from "@/src/lib/apis/order/create/OrderCreate.types";
+import { IOrderHistoryResponse } from "@/src/lib/apis/order/create/OrderCreate.types";
 import { getDate } from "@/src/lib/utils/utils";
+import { useQuery } from "@tanstack/react-query";
+import { OrderCreateApi } from "@/src/lib/apis/order/create/OrderCreateApi";
 
 interface ILoadOrderModalProps extends IModalProps {
   callback: (response: IOrderHistoryResponse) => void;
 }
 
 export default function LoadOrderModal(props: ILoadOrderModalProps) {
-
-  const searchBarArgs = useSearchbar(()=>{});
+  const searchBarArgs = useSearchbar(() => refetch());
   const [selectedId, setSelectedId] = useState<number>();
-  const data: IOrderHistory[] = [
-    {id: 0, name: "실리콘 부품 제작 프로젝트", imgUrl: "/images/netflix.webp", createdAt: "2023-08-30"},
-    {id: 1, name: "실리콘 부품 제작 프로젝트", imgUrl: "/images/netflix.webp", createdAt: "2023-08-30"},
-    {id: 2, name: "실리콘 부품 제작 프로젝트", imgUrl: "/images/netflix.webp", createdAt: "2023-08-30"}
-  ];
+
+  const { data, refetch } = useQuery({
+    queryKey: ["orderHistoryList"],
+    queryFn: () =>
+      OrderCreateApi.GET_ORDER_HISTORY_LIST(1, 5, searchBarArgs.keyword),
+  });
 
   const onLoad = () => {
-    props.callback({id: 0, name: "실리콘 부품 제작 프로젝트", manufacturingList: [], postProcessingList: null, drawingList: [], request: null, deliveryAddress: {id: 0, name:"집", zipCode: "1234", address: "주소", detailAddress: "상세 주소", receiver:"나", phone1: "010", phone2: null, isDefault: true}});
+    props.callback({
+      id: 0,
+      name: "실리콘 부품 제작 프로젝트",
+      manufacturingList: [],
+      postProcessingList: null,
+      drawingList: [],
+      request: null,
+      deliveryAddress: {
+        id: 0,
+        name: "집",
+        zipCode: "1234",
+        address: "주소",
+        detailAddress: "상세 주소",
+        receiver: "나",
+        phone1: "010",
+        phone2: null,
+        isDefault: true,
+      },
+    });
     props.onClose();
-  }
+  };
 
   return (
     <Modal isOpen={props.isOpen} onClose={props.onClose}>
@@ -40,26 +60,34 @@ export default function LoadOrderModal(props: ILoadOrderModalProps) {
           onSearchKeyword={searchBarArgs.onSearchKeyword}
         />
         <S.ModalContentWrapper>
-          {data.map((el)=> (
-            <S.LoadOrderItem className="flex-row-align-center" isSelect={el.id === selectedId} onClick={() => setSelectedId(el.id)}  key={el.id}>
-              <Image
-                width={100}
-                height={100}
-                src={el.imgUrl}
-                alt=""
-                style={S.LoadOrderItemImage}
-              />
-              <div>
-                <S.LoadOrderItemTitle className="medium24" isSelect={el.id === selectedId}>
-                  {el.name}
-                </S.LoadOrderItemTitle>
-                <S.LoadOrderItemDate className="medium16">
-                  {`거래 일자: ${getDate(el.createdAt)}`}
-                </S.LoadOrderItemDate>
-              </div>
-          </S.LoadOrderItem>
-          ))}
-          
+          {data &&
+            data.contents.map((el) => (
+              <S.LoadOrderItem
+                className="flex-row-align-center"
+                isSelect={el.id === selectedId}
+                onClick={() => setSelectedId(el.id)}
+                key={el.id}
+              >
+                <Image
+                  width={100}
+                  height={100}
+                  src={el.imgUrl}
+                  alt=""
+                  style={S.LoadOrderItemImage}
+                />
+                <div>
+                  <S.LoadOrderItemTitle
+                    className="medium24"
+                    isSelect={el.id === selectedId}
+                  >
+                    {el.name}
+                  </S.LoadOrderItemTitle>
+                  <S.LoadOrderItemDate className="medium16">
+                    {`거래 일자: ${getDate(el.createdAt)}`}
+                  </S.LoadOrderItemDate>
+                </div>
+              </S.LoadOrderItem>
+            ))}
         </S.ModalContentWrapper>
         <div className="flex-row">
           <S.CancelButton className="bold20" onClick={props.onClose}>
