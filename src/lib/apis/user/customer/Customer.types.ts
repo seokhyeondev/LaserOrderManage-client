b4a3ace6ed8480706c9dc24ce12de56e7558f4ef
안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>
2023-12-26 11:52:26 +0900

    #29 feat: api 관련 객체 선언

--- src/lib/apis/user/customer/Customer.types.ts
+++ src/lib/apis/user/customer/Customer.types.ts
@@ -16,6 +16,10 @@ export type IDeliveryAddressRequest = {
 
 export type IDeliveryAddressResponse = {};
 
+export type IEditDeliveryAddressResponse = {};
+
+export type IDeleteDeliveryAddressResponse = {};
+
 export type IDeliveryAddress = {
   id: number;
   name: string;
@@ -26,4 +30,24 @@ export type IDeliveryAddress = {
   phone1: string;
   phone2: string | null;
   isDefault: boolean;
+};
+
+export type ICustomerAccountResponse = {
+  email: string;
+  name: string;
+  phone: string;
+  zipCode: string;
+  address: string;
+  detailAddress: string | null;
+  companyName: string | null;
+  emailNotifiaction: boolean;
+};
+
+export type IEditCustomerAccountRequest = {
+  name: string;
+  phone: string;
+  zipCode: string;
+  address: string;
+  detailAddress: string | null;
+  companyName: string | null;
 };
---
+++ src/lib/apis/user/factory/Factory.types.ts
@@ -0,0 +1,49 @@
+import { IBaseListSimpleResponse } from "@/src/lib/apis/base/base.types";
+
+export type IFactoryResponse = {
+  email: string;
+  companyName: string;
+  representative: string;
+  phone: string;
+  fax: string;
+  zipCode: string;
+  address: string;
+  detailAddress: string | null;
+  emailNotification: boolean;
+};
+
+export type IEditFactoryRequest = {
+  companyName: string;
+  representative: string;
+  phone: string;
+  fax: string;
+  zipCode: string;
+  address: string;
+  detailAddress: string | null;
+};
+
+export type IEditFactoryResponse = {};
+
+export type IOrderManager = {
+  id: number;
+  name: string;
+  phone: string;
+};
+
+export type IOrderMangerListResponse = IBaseListSimpleResponse<IOrderManager>;
+
+export type IAddOrderMangerRequest = {
+  name: string;
+  phone: string;
+};
+
+export type IAddOrderManagerResponse = {};
+
+export type IEditOrderManagerRequest = {
+  name: string;
+  phone: string;
+};
+
+export type IEditOrderManagerResponse = {};
+
+export type IDeleteOrderManagerResponse = {};
