1b805a3b0dafe8ab4220b12db8bd29a8366de43b
안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>
2024-01-02 14:17:57 +0900

    #31 refactor: 새로고침 시 토큰 유지

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import type { AppProps } from "next/app";
+import type { AppContext, AppProps } from "next/app";
 import { Global } from "@emotion/react";
 import Layout from "@/src/components/commons/layout";
 import { globalStyles } from "@/src/styles/globalStyles";
@@ -8,19 +8,43 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import { RecoilEnv, RecoilRoot } from "recoil";
+import { MutableSnapshot, RecoilEnv, RecoilRoot } from "recoil";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { IToken } from "@/src/lib/apis/user/User.types";
+import { UserApi } from "@/src/lib/apis/user/UserApi";
+import { IAuthState, authState } from "@/src/store/auth";
+import { setCredentials } from "@/src/lib/utils/setCredentials";
+import App from "next/app";
 
 const roboto = Roboto({ weight: ["400", "500", "700"], subsets: ["latin"] });
 RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
-export default function App({ Component, pageProps }: AppProps) {
+type MyAppProps = AppProps & {
+  loginData: IToken | null;
+};
+
+function MyApp({ Component, pageProps, loginData }: MyAppProps) {
+  const initializer = ({ set }: MutableSnapshot) => {
+    if (loginData) {
+      const auth: IAuthState = {
+        isAuthenticated: true,
+        accessToken: loginData.accessToken,
+        role: loginData.role,
+      };
+      set(authState, auth);
+    }
+  };
+
+  useEffect(() => {
+    loginData && setCredentials(loginData);
+  }, [loginData]);
+
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <>
-      <RecoilRoot>
+      <RecoilRoot initializeState={initializer}>
         <QueryClientProvider client={queryClient}>
           <ReactQueryDevtools initialIsOpen={false} />
           <HydrationBoundary state={pageProps.dehydratedState} />
@@ -33,3 +57,19 @@ export default function App({ Component, pageProps }: AppProps) {
     </>
   );
 }
+
+MyApp.getInitialProps = async (context: AppContext) => {
+  const appProps = await App.getInitialProps(context);
+  let loginData: IToken | null;
+
+  try {
+    const token = await UserApi.REISSUE();
+    loginData = token;
+  } catch (error) {
+    loginData = null;
+  }
+
+  return { ...appProps, loginData };
+};
+
+export default MyApp;
--- src/lib/apis/axios.ts
+++ src/lib/apis/axios.ts
@@ -6,6 +6,7 @@ import axios, {
 } from "axios";
 import { UserApi } from "./user/UserApi";
 import { setCredentials } from "../utils/setCredentials";
+import { IToken } from "./user/User.types";
 
 const BASE_URL = "https://api.kumoh.org";
 
@@ -29,30 +30,38 @@ axiosPrivate.interceptors.response.use(
       return Promise.reject(error);
     }
 
-    const status = Number(error.response?.status);
-    const origin = error.config as AxiosRequestConfig;
+    if (error.response) {
+      const origin = error.config as AxiosRequestConfig;
+      const status = Number(error.response.status);
+      const errorData = error.response.data as IHttpStatus;
+      if (
+        status === 401 ||
+        (status === 400 && errorData.errorCode === "-001")
+      ) {
+        const newToken = await UserApi.REISSUE();
+        setCredentials(newToken);
+        const cookieString = makeCookieString(newToken);
+        axiosPrivate.defaults.headers.Cookie = cookieString;
+        (origin.headers as AxiosHeaders).set("set-cookie", cookieString);
+        (origin.headers as AxiosHeaders).set(
+          "Authorization",
+          `${newToken.grantType} ${newToken.accessToken}`,
+        );
 
-    if (status === 401) {
-      const newToken = await UserApi.REISSUE();
-      setCredentials(newToken);
-      const cookieString = `refreshToken=${newToken.refreshToken}; Path=/; domain:.kumoh.org; max-age=${newToken.refreshToken}; secure=true; SameSite=None`;
-      axiosPrivate.defaults.headers.Cookie = cookieString;
-      (origin.headers as AxiosHeaders).set("Set-Cookie", cookieString);
-      (origin.headers as AxiosHeaders).set(
-        "Authorization",
-        `${newToken.grantType} ${newToken.accessToken}`,
-      );
-
-      return axios(origin);
+        return axios(origin);
+      }
     }
 
     return Promise.reject(error);
   },
 );
 
+export const makeCookieString = (token: IToken) => {
+  return `refreshToken=${token.refreshToken}; Path=/; domain:.kumoh.org; max-age=${token.refreshToken}; secure=true; SameSite=None`;
+};
+
 export interface IHttpStatus {
   errorCode: string;
   httpStatus: string;
-  status: number;
   message: string;
 }
