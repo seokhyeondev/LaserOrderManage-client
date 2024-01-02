06e5f7e45835e8ff7340869548b42467a42006cc
안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>
2024-01-02 16:35:31 +0900

    #31 refactor: 새로고침 시 인증상태 유지

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,7 @@ import { UserApi } from "@/src/lib/apis/user/UserApi";
 import { IAuthState, authState } from "@/src/store/auth";
 import { setCredentials } from "@/src/lib/utils/setCredentials";
 import App from "next/app";
+import { axiosPrivate, makeCookieString } from "@/src/lib/apis/axios";
 
 const roboto = Roboto({ weight: ["400", "500", "700"], subsets: ["latin"] });
 RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
@@ -60,11 +61,17 @@ function MyApp({ Component, pageProps, loginData }: MyAppProps) {
 
 MyApp.getInitialProps = async (context: AppContext) => {
   const appProps = await App.getInitialProps(context);
+  const { ctx } = context;
   let loginData: IToken | null;
 
   try {
-    const token = await UserApi.REISSUE();
-    loginData = token;
+    if (ctx.req) {
+      const cookie = ctx.req.headers.cookie;
+      axiosPrivate.defaults.headers.Cookie = cookie || "";
+      const token = await UserApi.REISSUE();
+      loginData = token;
+      ctx.res?.setHeader("set-cookie", makeCookieString(token));
+    } else throw new Error("isClient");
   } catch (error) {
     loginData = null;
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { useMutation } from "@tanstack/react-query";
 export default function Home() {
   const { mutate } = useMutation({
     mutationFn: UserApi.REISSUE,
+    retry: 0,
   });
   return <a onClick={() => mutate()}>hello</a>;
 }
