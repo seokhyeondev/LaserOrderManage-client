e4ce40931d3d5fb22977c687ba71153afb1e39a2
안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>
2024-01-03 11:45:29 +0900

    #31 refactor: 새로고침 시 인증 상태 유지

---
+++ src/components/units/auth/AuthInitializer.index.tsx
@@ -0,0 +1,30 @@
+import { axiosPrivate } from "@/src/lib/apis/axios";
+import { UserType, authState } from "@/src/store/auth";
+import { getCookie } from "cookies-next";
+import { useEffect } from "react";
+import { useSetRecoilState } from "recoil";
+
+interface IAuthInitializerProps {
+  children: React.JSX.Element;
+}
+
+export default function AuthInitializer({ children }: IAuthInitializerProps) {
+  const setAuth = useSetRecoilState(authState);
+
+  useEffect(() => {
+    const accessToken = getCookie("accessToken");
+    const role = getCookie("role");
+    if (accessToken && role) {
+      setAuth({
+        isAuthenticated: true,
+        accessToken: accessToken,
+        role: role as UserType,
+      });
+      axiosPrivate.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${accessToken}`;
+    }
+  }, [setAuth]);
+
+  return <>{children}</>;
+}
