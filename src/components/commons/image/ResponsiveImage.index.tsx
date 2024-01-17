3dba3b3ff1a81c23b378c82215e3a6784a96fd42
안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>
2024-01-17 14:30:30 +0900

    #refactor: 메인 로고 이미지 최적화

--- src/components/commons/image/ResponsiveImage.index.tsx
+++ src/components/commons/image/ResponsiveImage.index.tsx
@@ -1,11 +1,14 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 import { ReactNode } from "react";
 
 interface IResponsiveImageProps {
-  src: string;
+  src: string | StaticImport;
   alt: string;
+  priority?: boolean;
+  blur?: boolean;
   Container: React.ComponentType<{ children: ReactNode }>;
 }
 
@@ -18,13 +21,25 @@ const StyledImage = styled(Image)`
   object-fit: cover;
 `;
 
-const ResponsiveImage = ({ src, alt, Container }: IResponsiveImageProps) => {
+const ResponsiveImage = ({
+  src,
+  alt,
+  Container,
+  priority,
+  blur,
+}: IResponsiveImageProps) => {
   const StyledContinaer = styled(Container)`
     ${defaultContainerStyle}
   `;
   return (
     <StyledContinaer>
-      <StyledImage src={src} alt={alt} layout="fill" />
+      <StyledImage
+        src={src}
+        alt={alt}
+        fill
+        priority={priority}
+        placeholder={blur ? "blur" : undefined}
+      />
     </StyledContinaer>
   );
 };
